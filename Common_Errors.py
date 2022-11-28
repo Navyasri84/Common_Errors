3cc76ead9a3f0cf36cae905c64e2487d44f41fc8
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-11-28 19:52:58 +0530

    Add files via upload

---
+++ Common_Errors.py
@@ -0,0 +1,7 @@
+# print(3/0) # ZeroDivisionError
+# print(hh) # NameError
+# a="hello"
+# print(int(a)) # ValueError
+# b=4.5
+# print(b+"Hello") # TypeError
+# print(['hello'].lower()) # AttributeError
